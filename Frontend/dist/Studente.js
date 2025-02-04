905079a0b1d3203657ee82513fac287e6a67e4f6
vnfortunato <vnfortunato@codearchitects.com>
2025-02-04 10:20:24 +0100

    removed dist folder

--- Frontend/dist/Studente.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Studente extends Persona {
-    GetNominativo() {
-        return `Studente: ${super.GetNominativo()}`;
-    }
-    get email() {
-        return (this.nome).slice(0, 1).toLowerCase() + this.cognome.toLowerCase() + "@" + "Studente".toLowerCase() + ".com";
-    }
-}
